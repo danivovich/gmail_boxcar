349055e5743b8cb41b26c425e9f85c92bd4e1cfa
Dan Ivovich <divovich@gmail.com>
2012-03-21 07:07:27 -0400

    handle multiple accounts

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-require "rubygems"
-require "bundler/setup"
+require 'rubygems'
+require 'bundler/setup'
 
 require 'set'
 require 'boxcar_api'
@@ -8,37 +8,53 @@ require 'active_support/all'
 
 POLL_INTERVAL = ENV['POLL_INTERVAL'] || 30
 
-username = ENV['GMAIL_USERNAME']
-password = ENV['GMAIL_PASSWORD']
-boxcar_key = ENV['BOXCAR_KEY']
-boxcar_secret = ENV['BOXCAR_SECRET']
+def get_account_info
+  accounts = {}
+  count = ENV['ACCOUNT_COUNT'] || 1
+  count = count.to_i
+  if count == 1
+    acounts[ENV['GMAIL_USERNAME']] = ENV['GMAIL_PASSWORD']
+    NOTIFY_ACCOUNT = ENV['GMAIL_USERNAME']
+  else
+    count.times do |i|
+      acounts[ENV["GMAIL_USERNAME_#{i}"]] = ENV["GMAIL_PASSWORD_#{i}"]
+    end
+    NOTIFY_ACCOUNT = ENV['GMAIL_USERNAME_0']
+  end
+  accounts
+end
 
-unless username.present? && password.present?
-  puts "GMAIL_USERNAME and GMAIL_PASSWORD env variables are not set"
+username_password_hash = get_account_info
+unless username_password_hash.length > 0
+  puts 'GMAIL_USERNAME and GMAIL_PASSWORD env variables are not set'
   exit
 end
 
+boxcar_key = ENV['BOXCAR_KEY']
+boxcar_secret = ENV['BOXCAR_SECRET']
 unless boxcar_key.present? && boxcar_secret.present?
-  puts "BOXCAR_KEY and BOXCAR_SECRET env variables are not set"
+  puts 'BOXCAR_KEY and BOXCAR_SECRET env variables are not set'
   exit
 end
 
 
 while true
-  puts "Checking gmail"
-  gmail = Gmail.new(username, password)
-  emails = gmail.inbox.emails(:unread)
-  emails.each do |e|
-    from = e.from.first.name
-    msg = e.subject
-    puts "  #{from}: #{msg}"
-    provider = BoxcarAPI::Provider.new(boxcar_key, boxcar_secret)
-    provider.notify(username, msg,
-      :from_screen_name => from,
-      :from_remote_service_id => e.uid)
+  username_password_hash.each_pair do |username, password|
+    puts "Checking #{username} gmail"
+    gmail = Gmail.new(username, password)
+    emails = gmail.inbox.emails(:unread)
+    emails.each do |e|
+      from = e.from.first.name
+      msg = e.subject
+      puts "  #{from}: #{msg}"
+      provider = BoxcarAPI::Provider.new(boxcar_key, boxcar_secret)
+      provider.notify(NOTIFY_ACCOUNT, msg,
+        :from_screen_name => from,
+        :from_remote_service_id => e.uid)
+    end
+    gmail.logout
+    puts 'Done checking, sleeping now'
+    $stdout.flush
   end
-  gmail.logout
-  puts "Done checking, sleeping now"
-  $stdout.flush
   sleep POLL_INTERVAL
 end
